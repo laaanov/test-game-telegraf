53d4e22068d02180547e0757fdb01fef4e8839cc
laaanov <ilyalaaanov@gmail.com>
2022-06-20 03:46:13 +0800

    Test

--- _token.js
+++ _token.js
@@ -1,8 +1,8 @@
 module.exports = {
     APP_NAME : "GameTest_Flappy",
     BOT_TOKEN : "5494707928:AAFujbekNwEBa7soMvkDR7FjFmVc0ntdvPY",
-    WEBHOOK : 'https://myawesomeurl.whatever',
-    GAME_URL : 'https://mycoolgame.gg',
+    WEBHOOK : 'https://test-game-telegraf.vercel.app/',
+    GAME_URL : 'https://test-game-telegraf.vercel.app/html',
     GAME_NAME : 'bird',
     PORT : 3000,
     DEFAULT_PORT : 3000
