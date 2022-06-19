f27283416a4d5164b4cd00fa270c7073a97f0303
Ilya Lanov <42113015+laaanov@users.noreply.github.com>
2022-06-20 07:49:34 +0800

    Update _token.js

--- _token.js
+++ _token.js
@@ -1,8 +1,8 @@
 module.exports = {
     APP_NAME : "GameTest_Flappy",
     BOT_TOKEN : "5494707928:AAFujbekNwEBa7soMvkDR7FjFmVc0ntdvPY",
-    WEBHOOK : 'https://test-game-telegraf.vercel.app/',
-    GAME_URL : 'https://test-game-telegraf.vercel.app/html',
+    WEBHOOK : 'https://flappy-test-bot.herokuapp.com/',
+    GAME_URL : 'https://flappy-test-bot.herokuapp.com/html',
     GAME_NAME : 'bird',
     PORT : 3000,
     DEFAULT_PORT : 3000
